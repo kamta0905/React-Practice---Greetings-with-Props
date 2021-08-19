31511676ad5ceda2b434c43ab9ae86a7ad87b41e
kamta0905 <88268903+kamta0905@users.noreply.github.com>
2021-08-19 23:46:52 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,13 +1,13 @@
 // write code for Welcome component here
-import React from 'react';
+import React from 'react'
 
 const Welcome = () => {
     return (
-        <section>
-        <h1>Hey!</h1>
-        <h2>Welcome to the Newton School</h2>
-        </section>
+        <div>
+            <h1>Hey!</h1>
+            
+        </div>
     )
-};
+}
 
 export default Welcome;
